387b8029460c31304c8071d46d7b92a0b9bcb607
kbougy <kyriayxvanderpoel@gmail.com>
2018-05-08 20:55:38 -0500

    Update handler to send SNS notification

--- handlers.py
+++ handlers.py
@@ -6,17 +6,26 @@ import mimetypes
 
 def lambda_handler(event, context):
     s3 = boto3.resource('s3', region_name='us-east-1')
-    site_bucket = s3.Bucket('site.kyrivanderpoel.com')
-    build_bucket = s3.Bucket('sitebuild.kyrivanderpoel.com')
+    sns = boto3.resource('sns', region_name='us-east-1')
+    topic = sns.Topic('arn:aws:sns:us-east-1:571996724366:deploySiteTopic')
 
-    obj = build_bucket.Object('build.zip')
-    site_zip = io.BytesIO()
-    obj.download_fileobj(site_zip)
+    try:
+        site_bucket = s3.Bucket('site.kyrivanderpoel.com')
+        build_bucket = s3.Bucket('sitebuild.kyrivanderpoel.com')
 
-    with zipfile.ZipFile(site_zip) as z:
-        for name in z.namelist():
-            site_bucket.upload_fileobj(z.open(name), name, ExtraArgs={
-                'ContentType': mimetypes.guess_type(name)[0]
-            })
-            site_bucket.Object(name).Acl().put(ACL='public-read')
-    return 'Hello from Lambda'
+        obj = build_bucket.Object('build.zip')
+        site_zip = io.BytesIO()
+        obj.download_fileobj(site_zip)
+
+        with zipfile.ZipFile(site_zip) as z:
+            for name in z.namelist():
+                site_bucket.upload_fileobj(z.open(name), name, ExtraArgs={
+                    'ContentType': mimetypes.guess_type(name)[0]
+                })
+                site_bucket.Object(name).Acl().put(ACL='public-read')
+        topic.publish(Subject='Site Deploy Success',
+                      Message='Site Successfully Deployed')
+    except:
+        topic.publish(Subject='Site Deploy Failed',
+                      Message='The portfolio was not created successfully')
+        raise
