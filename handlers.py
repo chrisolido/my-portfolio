c345198ee767e18950d92d1b5b8b0231189a80e4
kbougy <kyriayxvanderpoel@gmail.com>
2018-05-08 21:49:33 -0500

    Update handler

--- handlers.py
+++ handlers.py
@@ -3,17 +3,27 @@ import io
 import zipfile
 import mimetypes
 
-
 def lambda_handler(event, context):
     s3 = boto3.resource('s3', region_name='us-east-1')
     sns = boto3.resource('sns', region_name='us-east-1')
     topic = sns.Topic('arn:aws:sns:us-east-1:571996724366:deploySiteTopic')
 
+    location = {
+        'bucketName': 'sitebuild.kyrivanderpoel.com',
+        'objectKey': 'build.zip'
+    }
     try:
-        site_bucket = s3.Bucket('site.kyrivanderpoel.com')
-        build_bucket = s3.Bucket('sitebuild.kyrivanderpoel.com')
+        job = event.get("CodePipeline.job")
+        if job:
+            for artifact in job['data']['inputArtifacts']:
+                if artifact['name'] == "MyAppBuild":
+                    location = artifact['location']['s3Location']
 
-        obj = build_bucket.Object('build.zip')
+        print("Building portfolio from {}".format(str(location)))
+        site_bucket = s3.Bucket('site.kyrivanderpoel.com')
+        build_bucket = s3.Bucket(location['bucketName'])
+
+        obj = build_bucket.Object(location['objectKey'])
         site_zip = io.BytesIO()
         obj.download_fileobj(site_zip)
 
@@ -23,9 +33,13 @@ def lambda_handler(event, context):
                     'ContentType': mimetypes.guess_type(name)[0]
                 })
                 site_bucket.Object(name).Acl().put(ACL='public-read')
-        topic.publish(Subject='Site Deploy Success',
-                      Message='Site Successfully Deployed')
+
+        message = 'Site Successfully Deployed Manually'
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobId=job['id'])
+            message = "Site successfully deployed for CodePipeline job {}".format(job['id'])
+        topic.publish(Subject='Site Deploy Success', Message=message)
     except:
-        topic.publish(Subject='Site Deploy Failed',
-                      Message='The portfolio was not created successfully')
+        topic.publish(Subject='Site Deploy Failed', Message='The portfolio was not created successfully')
         raise
