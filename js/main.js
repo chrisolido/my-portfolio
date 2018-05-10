1af5641d6259fa95d8d39baa50fbf8fb1642adcf
kbougy <kyriayxvanderpoel@gmail.com>
2018-05-09 19:55:51 -0500

    Move HTML to React component

--- js/main.js
+++ js/main.js
@@ -1,4 +1,32 @@
-import 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log("Loaded react-dom");
-console.log("Webpack works");
+import ExampleWork from './example-work';
+
+const myWork = [
+  {
+    title: "CoolPal",
+    image: {
+      desc: "coolpal a library for building bots for discord",
+      src: "images/example1.png",
+      comment: ""
+    }
+  },
+  {
+    title: "YourPal",
+    image: {
+      desc: "yourpal hosted discord bot",
+      src: "./images/example2.png",
+      comment: ""
+    }
+  },
+  {
+    title: "Professional Portfolio",
+    image: {
+      desc: "professional portfolio",
+      src: "./images/example3.png",
+      comment: ""
+    }
+  },
+];
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
